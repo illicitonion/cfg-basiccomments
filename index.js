00a457073d3024600f11cc76e4599046cbc75163
Daniel Wagner-Hall <dawagner@gmail.com>
2018-11-25 13:12:53 +0000

    Clean up, handle some errors

--- index.js
+++ index.js
@@ -1,22 +1,61 @@
-var sqlite3 = require('sqlite3').verbose();
+// sqlite3 documentation: https://github.com/mapbox/node-sqlite3/wiki/API
+var sqlite3 = require('sqlite3');
 var http = require('http');
 var url = require('url');
 var escape = require('escape-html');
 
+// Error code from https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+var internalServerError = 500;
+
 var db = new sqlite3.Database('posts.db');
 db.run("CREATE TABLE IF NOT EXISTS posts (id INT PRIMARY KEY, name TEXT, body TEXT)");
 
-http.createServer((req, res) => {
-  var u = url.parse(req.url, true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  if (u.pathname === "/post") {
-      var stmt = db.prepare("INSERT INTO posts (name, body) VALUES (?, ?)");
-      stmt.run([escape(u.query.name), escape(u.query.body)], () => { res.end(); });
-      stmt.finalize();
+// callback will be called with an error which may be null.
+function insertPost(name, body, callback) {
+  var statement = db.prepare("INSERT INTO posts (name, body) VALUES (?, ?)");
+  statement.run(
+    [escape(name), escape(body)],
+    callback
+  );
+  statement.finalize();
+}
+
+// callback will be called with (error, result) where error may be null, and result will be a dictionary.
+function listPosts(callback) {
+  var result = [];
+  db.each(
+    "SELECT name, body FROM posts ORDER BY id DESC",
+    (error, row) => { result.push({"name": row.name, "body": row.body}); },
+    (error) => callback(error, result),
+  );
+}
+
+function handle(request, response) {
+  response.setHeader("Access-Control-Allow-Origin", "*");
+
+  var requestUrl = url.parse(request.url, true);
+  if (requestUrl.pathname === "/add-new-post") {
+    insertPost(
+      requestUrl.query.name,
+      requestUrl.query.body,
+      (error) => {
+        if (error) {
+          console.log("Error inserting: " + error);
+          response.statusCode = internalServerError;
+        }
+        response.end();
+      }
+    );
   } else {
-    var result = [];
-    db.each("SELECT name, body FROM posts ORDER BY id DESC", function(err, row) {
-      result.push({"name": row.name, "body": row.body});
-    }, () => { res.write(JSON.stringify(result)); res.end() });
+    listPosts((error, result) => {
+      if (error) {
+        console.log("Error selecting: " + error);
+        response.statusCode = internalServerError;
+      }
+      response.write(JSON.stringify(result));
+      response.end();
+    });
   }
-}).listen(8080);
+}
+
+http.createServer(handle).listen(8080);
